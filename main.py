aefe5a35ceb576e532588f1df1c3c4c11f8289de
jpedrofsilva <123392443+jpedrofsilva@users.noreply.github.com>
2023-01-24 13:56:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-## This script downloads in bulk NASA's image of the day to a local directory
+## This script downloads NASA's image of the day in bulk to a local directory
 
 import requests
 import json
@@ -8,9 +8,10 @@ import ssl
 import os
 from dotenv import load_dotenv
 
+#load .env file for importing API Key
 load_dotenv()
 
-#had to bypass ssl certification for https downloads to work
+#bypass ssl certification for https downloads to work
 ssl._create_default_https_context = ssl._create_unverified_context
 
 #API URL
@@ -31,18 +32,19 @@ params = {
     'hd':'True',
     'api_key': key
     }
+
 #Store raw response data
 response = requests.get(url, params=params)
 
 #Convert response data to json
 json_data = response.json()
 
-#Iterate thru each dictionary in the json_data and downloads each image
+#Iterate thru each dictionary in the json_data and download each image
 for i in range(len(json_data)):
     #try needs to be used because some responses don't have a URL, this catches the error and continues the loop
     try:
-        temp_url = json_data[i]['hdurl'] #stores image url in a str
-        temp_directory = directory + "/" + str(json_data[i]['date']) + ".jpg" #defines the directory to store each image
+        temp_url = json_data[i]['hdurl'] #stores image url in a string
+        temp_directory = directory + "/" + str(json_data[i]['date']) + ".jpg" #defines the directory, file name, and file extension
         urllib.request.urlretrieve(temp_url, temp_directory) #downloads the image using the url and stores in directory
         print(json_data[i]['date']+" downloaded successfully")
         i=i+1
