3b2bafbcc3a5e4c6d821240a11e0a977a806463e
Pedro Silva <email.jpedrosilva@gmail.com>
2023-01-24 13:46:38 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,31 @@ import webbrowser
 import urllib.request
 import ssl
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 #had to bypass ssl certification for https downloads to work
 ssl._create_default_https_context = ssl._create_unverified_context
 
 #API URL
-url= 'https://api.nasa.gov/planetary/apod'
+url = 'https://api.nasa.gov/planetary/apod'
 #API Key
-key= os.getenv(API_KEY)
+key = os.getenv("API_KEY")
+
+print("NASA Image Dump Script")
+
+start_date = input("Input Start Date in YYYY-MM-DD:")
+end_date = input("Input End Date in YYYY-MM-DD:")
+directory = input("Input Download Directory:")
 
 #API Parameters
 params = {
-    'start_date':'2022-01-21',
-    'end_date':'2022-01-31',
+    'start_date': start_date,
+    'end_date': end_date,
     'hd':'True',
     'api_key': key
     }
-
 #Store raw response data
 response = requests.get(url, params=params)
 
@@ -34,9 +42,9 @@ for i in range(len(json_data)):
     #try needs to be used because some responses don't have a URL, this catches the error and continues the loop
     try:
         temp_url = json_data[i]['hdurl'] #stores image url in a str
-        directory = "C:/Users/jps80/Downloads/NASADUMP/" + str(json_data[i]['date']) + ".jpg" #defines the directory to store each image
-        urllib.request.urlretrieve(temp_url, directory) #downloads the image using the url and stores in directory
-        print(json_data[i]['date']+" success")
+        temp_directory = directory + "/" + str(json_data[i]['date']) + ".jpg" #defines the directory to store each image
+        urllib.request.urlretrieve(temp_url, temp_directory) #downloads the image using the url and stores in directory
+        print(json_data[i]['date']+" downloaded successfully")
         i=i+1
     except:
-        print(json_data[i]['date']+" No img found") #error handling
+        print(json_data[i]['date']+" No image found") #error handling
